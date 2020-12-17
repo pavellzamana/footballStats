f6d96f46ab251f41cea4af5dc9ad504070d27ac7
PavelZamana <pavell.zamana@gmail.com>
2020-12-17 17:00:24 +0300

    icon refactored to object

--- src/components/details/MatchDetail/MatchDetail.tsx
+++ src/components/details/MatchDetail/MatchDetail.tsx
@@ -10,21 +10,6 @@ import {setFixture} from "../../../redux/actions";
 const MatchDetail: React.FC = () => {
     const fixtureDetails = useSelector((state: AppStateType) => state.details.fixture);
     const featuredResults = useSelector((state: AppStateType) => state.details.featuredResults);
-    const icon = (event: IEvents) => {
-        switch (event.type) {
-            case "Goal":
-                return <img src={"https://www.pinclipart.com/picdir/big/218-2187811_thank-you-to-our-volunteers-and-sponsors-soccer.png"} alt={'Goal'}
-                            className={style.icon}/>
-            case "Card": {
-                return <img src={"https://upload.wikimedia.org/wikipedia/commons/f/fb/Yellow_card_icon.svg"}
-                            alt={'Card'}
-                            className={style.icon}/>
-            }
-            case "subst":
-                return <img src={"http://cdn.onlinewebfonts.com/svg/img_344041.png"} alt={'Sub'}
-                            className={style.icon}/>
-        }
-    }
     const dispatch = useDispatch();
     const setFixtureData = (id: number) => {
         dispatch(setFixture(id));
@@ -32,6 +17,18 @@ const MatchDetail: React.FC = () => {
     let homeEvents: Array<IEvents>;
     let awayEvents: Array<IEvents>;
 
+    const icon: any = {
+        Goal: <img
+            src={"https://www.pinclipart.com/picdir/big/218-2187811_thank-you-to-our-volunteers-and-sponsors-soccer.png"}
+            alt={'Goal'}
+            className={style.icon}/>,
+        Card: <img src={"https://upload.wikimedia.org/wikipedia/commons/f/fb/Yellow_card_icon.svg"}
+                   alt={'Card'}
+                   className={style.icon}/>,
+        subst: <img src = {"http://cdn.onlinewebfonts.com/svg/img_344041.png"} alt = {'Sub'}
+                    className = {style.icon}
+        />
+    }
     if (fixtureDetails.events) {
         homeEvents = fixtureDetails.events.filter((event: IEvents) => event.team_id === fixtureDetails.homeTeam.team_id);
         awayEvents = fixtureDetails.events.filter((event: IEvents) => event.team_id === fixtureDetails.awayTeam.team_id);
@@ -61,11 +58,11 @@ const MatchDetail: React.FC = () => {
             {fixtureDetails.events ?
             <div className={style.away}>
                 <div>{homeEvents!.map((event: IEvents) =>
-                    <Col span={24}>{event.elapsed}. {icon(event)} {event.type === 'subst' ?
+                    <Col span={24}>{event.elapsed}. {icon[`${event.type}`]} {event.type === 'subst' ?
                         event.player + '>' + event.assist : event.player}</Col>
                 )}</div>
                 <div>{awayEvents!.map((event: IEvents) =>
-                    <Col span={24}>{event.elapsed}. {icon(event)} {event.type === 'subst' ?
+                    <Col span={24}>{event.elapsed}. {icon[`${event.type}`]} {event.type === 'subst' ?
                         event.player + '>' + event.assist : event.player}</Col>
                 )}</div>
             </div> : <b>Match Postponed</b>}
